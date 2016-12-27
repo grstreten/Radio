537d0fabe403472b8c1c084e30be8f278f6c08ac
George Streten <gstreten@gmail.com>
2016-12-27 21:52:56 +0000

    Name update + app
    Added app tiles and updated name to suit…keeping the GH name and
    published name the same

--- Radio/ViewController.swift
+++ Radio/ViewController.swift
@@ -12,17 +12,16 @@ import MediaPlayer
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
-    let stations : [[String:String]] = [["name":"BBC R1","url":"http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p","artwork":"BBCR1Artwork","caption":"United Kingdom"],
-                                        ["name":"BBC R2","url":"http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio2_mf_p","artwork":"BBCR2Artwork","caption":"United Kingdom"],
-                                        ["name":"Mix 94.5","url":"http://stream.scahw.com.au/live/6mix_128.stream/playlist.m3u8","artwork":"MixArtwork","caption":"Perth, WA"],
-                                        ["name":"Coast FM","url":"http://stream.coastlive.com.au","artwork":"CoastArtwork","caption":"Perth, WA"]]
-
-    @IBOutlet weak var timeLabel: UILabel!
-    
-    @IBOutlet weak var collectionViewOutlet: UICollectionView!
-    
+    let tiles : [[String:String]] = [["name":"BBC R1","url":"http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p","artwork":"BBCR1Artwork","caption":"United Kingdom","type":"radio"],
+                                        ["name":"BBC R2","url":"http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio2_mf_p","artwork":"BBCR2Artwork","caption":"United Kingdom","type":"radio"],
+                                        ["name":"Mix 94.5","url":"http://stream.scahw.com.au/live/6mix_128.stream/playlist.m3u8","artwork":"MixArtwork","caption":"Perth, WA","type":"radio"],
+                                        ["name":"Coast FM","url":"http://stream.coastlive.com.au","artwork":"CoastArtwork","caption":"Perth, WA","type":"radio"],
+                                        ["name":"Spotify","url":"spotify://","artwork":"SpotifyArtwork","type":"app"],
+                                        ["name":"Waze","url":"waze://","artwork":"WazeArtwork","type":"app"]]
     var player = AVPlayer()
     var playingIndex = 99
+    
+    @IBOutlet weak var collectionViewOutlet: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,16 +45,10 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         })
         
         updateUI()
-        _ = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(self.updateUI), userInfo: nil, repeats: true)
+        Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(self.updateUI), userInfo: nil, repeats: true)
     }
     
     func updateUI(){
-        //Time
-        let formatter = DateFormatter()
-        formatter.dateFormat = "HH:mm"
-        let date = Date()
-        timeLabel.text = formatter.string(from: date)
-        
         if playingIndex != 99 {
             if player.timeControlStatus == AVPlayerTimeControlStatus.paused {
                 let pauseItemView = collectionViewOutlet.visibleCells[playingIndex].viewWithTag(2) as! UIImageView
@@ -68,7 +61,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func playItemAtIndex(index: Int){
-        let station = stations[index]
+        let station = tiles[index]
         
         let url = station["url"]!
         let playerItem = AVPlayerItem(url:NSURL(string: url) as! URL)
@@ -91,10 +84,6 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         ]
     }
 
-    @IBAction func openMap(_ sender: Any) {
-        UIApplication.shared.open(NSURL(string: "waze://") as! URL, options: [:], completionHandler: nil)
-    }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -109,41 +98,45 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let station = stations[indexPath.row]
-        NSLog("Tapped \(station["name"]!)")
-        
-        let tappedCell = collectionView.cellForItem(at: indexPath)
-        
-        if playingIndex == 99 { //Then play something
-            let pauseItemView = tappedCell?.viewWithTag(2) as! UIImageView
-            pauseItemView.alpha = 1.0
+        let tile = tiles[indexPath.row]
+        NSLog("Tapped \(tile["name"]!)")
+
+        if tile["type"] == "radio"{
+            let tappedCell = collectionView.cellForItem(at: indexPath)
             
-            playItemAtIndex(index: indexPath.row)
-            playingIndex = indexPath.row
-        } else if indexPath.row == playingIndex { //Then stop playing
-            for cell in collectionView.visibleCells{
-                let pauseItemView = cell.viewWithTag(2) as! UIImageView
-                pauseItemView.alpha = 0.0
+            if playingIndex == 99 { //Then play something
+                let pauseItemView = tappedCell?.viewWithTag(2) as! UIImageView
+                pauseItemView.alpha = 1.0
+                
+                playItemAtIndex(index: indexPath.row)
+                playingIndex = indexPath.row
+            } else if indexPath.row == playingIndex { //Then stop playing
+                for cell in collectionView.visibleCells{
+                    let pauseItemView = cell.viewWithTag(2) as! UIImageView
+                    pauseItemView.alpha = 0.0
+                }
+                playingIndex = 99
+                player.replaceCurrentItem(with: nil)
+                MPNowPlayingInfoCenter.default().nowPlayingInfo = nil
+            } else if indexPath.row != playingIndex { //Then change what is playing
+                for cell in collectionView.visibleCells{
+                    let pauseItemView = cell.viewWithTag(2) as! UIImageView
+                    pauseItemView.alpha = 0.0
+                }
+                
+                let pauseItemView = tappedCell?.viewWithTag(2) as! UIImageView
+                pauseItemView.alpha = 1.0
+                
+                playItemAtIndex(index: indexPath.row)
+                playingIndex = indexPath.row
             }
-            playingIndex = 99
-            player.pause()
-            MPNowPlayingInfoCenter.default().nowPlayingInfo = nil
-        } else if indexPath.row != playingIndex { //Then change what is playing
-            for cell in collectionView.visibleCells{
-                let pauseItemView = cell.viewWithTag(2) as! UIImageView
-                pauseItemView.alpha = 0.0
-            }
-            
-            let pauseItemView = tappedCell?.viewWithTag(2) as! UIImageView
-            pauseItemView.alpha = 1.0
-            
-            playItemAtIndex(index: indexPath.row)
-            playingIndex = indexPath.row
+        } else if tile["type"] == "app"{
+            UIApplication.shared.open(NSURL(string: tile["url"]!) as! URL, options: [:], completionHandler: nil)
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return stations.count
+        return tiles.count
     }
     
     func collectionView(_ collectionView: UICollectionView,
@@ -156,7 +149,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         let item : UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "item", for: indexPath) as UICollectionViewCell
         
         let imageView = item.viewWithTag(1) as! UIImageView
-        imageView.image = UIImage(named: stations[indexPath.row]["artwork"]!)!
+        imageView.image = UIImage(named: tiles[indexPath.row]["artwork"]!)!
         
         return item
     }
