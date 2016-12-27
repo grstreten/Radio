205526848602a01da4443b9bc784d557dfeccc14
George <gstrets@streten.local>
2016-12-27 15:23:13 +0000

    Initial Commit

---
+++ Radio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Radio
+//
+//  Created by George on 27/12/2016.
+//  Copyright © 2016 George Streten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
